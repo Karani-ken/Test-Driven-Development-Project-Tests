b1981393aacda0643538100c7d7d7e64a6b3436c
karani-ken <academice79@gmail.com>
2023-08-24 17:29:38 +0300

    More tests

--- Processor/DeskBookingRequestProcessorTests.cs
+++ Processor/DeskBookingRequestProcessorTests.cs
@@ -1,37 +1,43 @@
-﻿using DeskBooker.Core.Dormain;
+﻿using DeskBooker.Core.DataInterface;
+using DeskBooker.Core.Dormain;
 using DeskBooker.Core.Processor;
+using Moq;
 
 namespace DeskBooker.Core.Tests.Processor
 {
     public class DeskBookingRequestProcessorTests
     {
+        private readonly DeskBookingRequest _request;
+        private readonly Mock<IDeskBookingRepository> _deskBookingRepositoryMock;
         private readonly DeskBookingRequestProcessor _processor;
 
         public DeskBookingRequestProcessorTests()
         {
-            _processor = new DeskBookingRequestProcessor();
-        }
-        [Fact]
-        public void ShouldReturnDeskBookingResultWithRequestValues()
-        {
-            //Arrange
-            var request = new DeskBookingRequest
+            _request = new DeskBookingRequest
             {
                 FirstName = "Kenneth",                                  /* setting up the  request data for the desk booking class*/
                 LastName = "Karani",
                 Email = "kenneth@KennethMwangiKarani.com",
                 Date = new DateTime(2023, 8, 24)
             };
+            _deskBookingRepositoryMock = new Mock<IDeskBookingRepository>();
+            _processor = new DeskBookingRequestProcessor(_deskBookingRepositoryMock.Object);
+        }
+        [Fact]
+        public void ShouldReturnDeskBookingResultWithRequestValues()
+        {
+            //Arrange
+           
             
             //Act
-            DeskBookingResult result = _processor.BookDesk(request);
+            DeskBookingResult result = _processor.BookDesk(_request);
 
             //Assert
             Assert.NotNull(result);
-            Assert.Equal(request.FirstName, result.FirstName); //if the FirstName from request in the name from the results FirstName also checks the other details like lastname,email and Date
-            Assert.Equal(request.LastName, result.LastName);
-            Assert.Equal(request.Email, result.Email);
-            Assert.Equal(request.Date, result.Date);
+            Assert.Equal(_request.FirstName, result.FirstName); //if the FirstName from request in the name from the results FirstName also checks the other details like lastname,email and Date
+            Assert.Equal(_request.LastName, result.LastName);
+            Assert.Equal(_request.Email, result.Email);
+            Assert.Equal(_request.Date, result.Date);
         }
 
         [Fact]
@@ -40,6 +46,23 @@ namespace DeskBooker.Core.Tests.Processor
             
             var exception = Assert.Throws<ArgumentNullException>(() => _processor.BookDesk(null));
             Assert.Equal("request", exception.ParamName);
+        }
+        [Fact]
+        public void ShouldSaveDeskBooking()
+        {
+            DeskBooking savedDeskBooking = null;
+            _deskBookingRepositoryMock.Setup(x => x.Save(It.IsAny<DeskBooking>()))
+                 .Callback<DeskBooking>(deskBooking =>
+                 {
+                     savedDeskBooking = deskBooking;
+                 });
+            _processor.BookDesk(_request);
+            _deskBookingRepositoryMock.Verify(x => x.Save(It.IsAny<DeskBooking>()), Times.Once);
+            Assert.NotNull(savedDeskBooking);
+            Assert.Equal(_request.FirstName, savedDeskBooking.FirstName);
+            Assert.Equal(_request.LastName, savedDeskBooking.LastName);
+            Assert.Equal(_request.Email, savedDeskBooking.Email);
+            Assert.Equal(_request.Date, savedDeskBooking.Date);
         }
     }
 }
