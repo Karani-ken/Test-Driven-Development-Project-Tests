77f81d6205b2fa81b5171289beeeb57d65eddb19
karani-ken <academice79@gmail.com>
2023-08-24 16:27:36 +0300

    Second Commit

---
+++ Processor/DeskBookingRequestProcessorTests.cs
@@ -0,0 +1,45 @@
+﻿using DeskBooker.Core.Dormain;
+using DeskBooker.Core.Processor;
+
+namespace DeskBooker.Core.Tests.Processor
+{
+    public class DeskBookingRequestProcessorTests
+    {
+        private readonly DeskBookingRequestProcessor _processor;
+
+        public DeskBookingRequestProcessorTests()
+        {
+            _processor = new DeskBookingRequestProcessor();
+        }
+        [Fact]
+        public void ShouldReturnDeskBookingResultWithRequestValues()
+        {
+            //Arrange
+            var request = new DeskBookingRequest
+            {
+                FirstName = "Kenneth",                                  /* setting up the  request data for the desk booking class*/
+                LastName = "Karani",
+                Email = "kenneth@KennethMwangiKarani.com",
+                Date = new DateTime(2023, 8, 24)
+            };
+            
+            //Act
+            DeskBookingResult result = _processor.BookDesk(request);
+
+            //Assert
+            Assert.NotNull(result);
+            Assert.Equal(request.FirstName, result.FirstName); //if the FirstName from request in the name from the results FirstName also checks the other details like lastname,email and Date
+            Assert.Equal(request.LastName, result.LastName);
+            Assert.Equal(request.Email, result.Email);
+            Assert.Equal(request.Date, result.Date);
+        }
+
+        [Fact]
+        public void ShouldThrowExceptionIfRequestIsNull()
+        {
+            
+            var exception = Assert.Throws<ArgumentNullException>(() => _processor.BookDesk(null));
+            Assert.Equal("request", exception.ParamName);
+        }
+    }
+}
